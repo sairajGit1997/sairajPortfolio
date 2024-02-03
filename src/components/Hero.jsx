ba54518b2f005120aeebec86d2bd50945d0c564a
sairajGit1997 <122759771+sairajGit1997@users.noreply.github.com>
2024-02-02 22:24:13 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Sairaj Kuchana</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop user
-            interfaces and web applications
+            I develop user interfaces and web applications.
           </p>
         </div>
       </div>
